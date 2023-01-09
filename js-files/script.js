3eb7c4eafe88c004b4aa5e038f53fb5e25c84c3a
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-01-09 19:54:50 +0000

    Add featured speakers dynamically to mobile index.html

--- js-files/script.js
+++ js-files/script.js
@@ -1,45 +1,58 @@
  const speakers = [
-    {speakerName: 'Glen Nii Noi Anum',
-    speakerImage: '',
-    speakerEducation: '',
-    speakerAccolades: ''
+    {speakerName: 'Trevoh Noah',
+    speakerImage: 'images/trevoh.jpg',
+    speakerEducation: 'Berkham Professor of Enterpreneurial Legal Studies at Harvard Law',
+    speakerAccolades: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
     },
-    {speakerName: 'Glen Nii Noi Anum',
-    speakerImage: '',
-    speakerEducation: '',
-    speakerAccolades: ''
+    {speakerName: 'Trevoh Noah',
+    speakerImage: 'images/trevoh.jpg',
+    speakerEducation: 'Berkham Professor of Enterpreneurial Legal Studies at Harvard Law',
+    speakerAccolades: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
     },
-    {speakerName: 'Glen Nii Noi Anum',
-    speakerImage: '',
-    speakerEducation: '',
-    speakerAccolades: ''
+    {speakerName: 'Trevoh Noah',
+    speakerImage: 'images/trevoh.jpg',
+    speakerEducation: 'Berkham Professor of Enterpreneurial Legal Studies at Harvard Law',
+    speakerAccolades: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
     },
-    {speakerName: 'Glen Nii Noi Anum',
-    speakerImage: '',
-    speakerEducation: '',
-    speakerAccolades: ''
+    {speakerName: 'Trevoh Noah',
+    speakerImage: 'images/trevoh.jpg',
+    speakerEducation: 'Berkham Professor of Enterpreneurial Legal Studies at Harvard Law',
+    speakerAccolades: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
     },
-    {speakerName: 'Glen Nii Noi Anum',
-    speakerImage: '',
-    speakerEducation: '',
-    speakerAccolades: ''
+    {speakerName: 'Trevoh Noah',
+    speakerImage: 'images/trevoh.jpg',
+    speakerEducation: 'Berkham Professor of Enterpreneurial Legal Studies at Harvard Law',
+    speakerAccolades: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
     },
-    {speakerName: 'Glen Nii Noi Anum',
-    speakerImage: '',
-    speakerEducation: '',
-    speakerAccolades: ''
-    },
+    {speakerName: 'Trevoh Noah',
+    speakerImage: 'images/trevoh.jpg',
+    speakerEducation: 'Berkham Professor of Enterpreneurial Legal Studies at Harvard Law',
+    speakerAccolades: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
+    }, 
  ]
 
 
-
+const speakersDiv = document.querySelector('.featured-speakers-div')
 const hamburger = document.querySelector('.hamburger')
 const navMenu = document.querySelector('.nav-links-ul')
 const navLinks = document.querySelectorAll('.nav-links')
 
-console.log(hamburger)
-console.log(navMenu)
-// 
+
+for (let i = 0; i < speakers.length; i++) {
+   speakersDiv.innerHTML += `
+   <div class="individual-speaker">
+      <div class="chessboard-and-speaker-image">
+         <img class="chess-board" src="images/chess-board-removebg-preview.png" alt="">
+         <div class="speaker-image"><img class="" src=${speakers[i].speakerImage} alt=""></div>
+      </div>
+      <div class="speaker-info">
+         <p class="speaker-name">${speakers[i].speakerName}</p>
+         <p class="speaker-education">${speakers[i].speakerEducation}</p>
+         <span class="span-black"></span>
+         <p class="speaker-accolades">${speakers[i].speakerAccolades}</p>
+      </div>
+   </div>`
+}
 
 
 hamburger.addEventListener('click', () => {
